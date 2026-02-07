1666f2378035bc3fe5c6faf8a62d2cde91a4a79d
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-07 10:28:45 -0500

    commit - PentominoSolver / .

--- src/model/Board.java
+++ src/model/Board.java
@@ -49,4 +49,4 @@ public class Board {
         }
         return sb.toString();
     }
-}   
+}
--- src/solver/PentominoSolver.java
+++ src/solver/PentominoSolver.java
@@ -1,4 +1,35 @@
 package solver;
 
+import model.Board;
+import model.Pentomino;
+
+import java.util.List;
+
 public class PentominoSolver {
-}
+
+    private int steps = 0;
+
+    public boolean solve(Board board, List<Pentomino> pieces, int index) {
+        if (index == pieces.size()) return true;
+
+        Pentomino p = pieces.get(index);
+
+        for (boolean[][] shape : p.getOrientations()) {
+            for (int r = 0; r < 10; r++) {
+                for (int c = 0; c < 10; c++) {
+                    steps++;
+                    if (board.canPlace(shape, r , c)) {
+                        board.place(shape, r, c, p.getName());
+                        if (solve(board, pieces, index + 1)) return true;
+                        board.remove(shape, r, c);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+}   
