c71d49d30c3a3cb7723d166231db01b2c23ce592
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-07 10:42:16 -0500

    commit - Main / .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import dao.GameResultDAO;
+import model.Board;
+import model.Pentomino;
+import solver.PentominoSolver;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Board board = new Board(6, 10);
+        PentominoSolver solver = new PentominoSolver();
+
+        long start = System.currentTimeMillis();
+     /* boolean solved = solver.solve(board, PentominoFactory.createAll(), 0);
+        long end = System.currentTimeMillis();
+
+        if (solved) {
+            System.out.println("✔ Solución encontrada");
+            System.out.println(board.serialize());
+
+            new GameResultDAO().save(
+                    6, 10,
+                    board.serialize(),
+                    solver.getSteps(),
+                    end - start
+            );
+        } else {
+            System.out.println("❌ No hay solución");
+        } */
     }
 }
